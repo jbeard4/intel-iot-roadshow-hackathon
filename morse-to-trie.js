e66f34a941838615f78e91f5e7cc1644b55c45f3
Jacob Beard <jbeard4@cs.mcgill.ca>
2015-05-10 06:32:52 -0400

    Filtered out all but characters. Used characters as state names

--- morse-to-trie.js
+++ morse-to-trie.js
@@ -8,7 +8,11 @@ function State(id,emit){
 
 var trieRoot = new State('idle');
 
+var re = /[a-z]/;
 Object.keys(morseCode).forEach(function(c){
+
+  if(!c.match(re)) return;    //only deal with letters for now
+
   var code = morseCode[c]; 
   var sequence = code.split('').map(function(x){ return x === '.' ? 'dot' : 'dash'});
 
@@ -22,7 +26,7 @@ Object.keys(morseCode).forEach(function(c){
       currentNode.transitions[s] = 
         currentNode.transitions[s] || new State(prefix);
   });
-  currentNode.transitions['short_pause'] = new State(prefix + '-terminal',c);   //followed by a dash, takes you to terminal
+  currentNode.transitions['short_pause'] = new State(c,c);   //followed by a dash, takes you to terminal
 
   //anything else takes you to a parser error
 });
