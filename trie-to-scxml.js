75717c32c8dbfc1d4a4ba451568a0afcc7d6f086
Jacob Beard <jbeard4@cs.mcgill.ca>
2015-05-10 05:34:50 -0400

    Working great. Successfully parses command and emits the proper character to _changes stream

--- trie-to-scxml.js
+++ trie-to-scxml.js
@@ -3,6 +3,9 @@
 //create a template that generates SCXML code
 
 var trie = require('./morse-to-trie');
+var fs = require('fs');
+
+var eventProcessorScxml = fs.readFileSync(__dirname + '/components/event_processor.scxml','utf8');
 
 var states = [];
 
@@ -27,18 +30,25 @@ walk(trie);
 var scxmlString = 
   '<?xml version="1.0" encoding="UTF-8"?>' + 
   '<scxml xmlns="http://www.w3.org/2005/07/scxml" name="morse" datamodel="ecmascript" version="1.0">' + 
-    states.map(function(state){
-      return '<state id="' + state.id + '">' + 
-        (
-          state.emit ? 
-            '<onentry><send type="http://scxml.io/scxmld" target="scxml://publish" event="character" contentexpr="\'' + state.emit + '\'"></send></onentry>' +
-            '<transition target="idle"/>' //go back to start 
-            : '' ) + 
-        state.transitions.map(function(t){
-          return '<transition target="' + t.target + '" event="' + t.event + '"/>' 
+    '<parallel id="main">' +
+      '<state id="event_processor">' +
+        eventProcessorScxml +
+      '</state>' +
+      '<state id="parsing_state">' +
+        states.map(function(state){
+          return '<state id="' + state.id + '">' + 
+            (
+              state.emit ? 
+                '<onentry><send type="http://scxml.io/scxmld" target="scxml://publish" event="character" contentexpr="\'' + state.emit + '\'"></send></onentry>' +
+                '<transition target="idle"/>' //go back to start 
+                : '' ) + 
+            state.transitions.map(function(t){
+              return '<transition target="' + t.target + '" event="' + t.event + '"/>' 
+            }).join('') + 
+        '</state>' 
         }).join('') + 
-    '</state>' 
-    }).join('') + 
+      '</state>' +
+    '</parallel>' +
   '</scxml>';
 
 console.log(scxmlString);
